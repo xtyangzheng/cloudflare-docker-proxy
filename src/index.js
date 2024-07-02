b1bb6d1c69678a6809d39d59ede99d730e6f0b18
杨峥 <xtyangzheng@163.com>
2024-07-02 14:37:35 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,17 +7,17 @@ const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
   // production
-  "docker.libcuda.so": dockerHub,
-  "quay.libcuda.so": "https://quay.io",
-  "gcr.libcuda.so": "https://gcr.io",
-  "k8s-gcr.libcuda.so": "https://k8s.gcr.io",
-  "k8s.libcuda.so": "https://registry.k8s.io",
-  "ghcr.libcuda.so": "https://ghcr.io",
-  "cloudsmith.libcuda.so": "https://docker.cloudsmith.io",
-  "ecr.libcuda.so": "https://public.ecr.aws",
+  "docker.qfcx.cn": dockerHub,
+  "quay.qfcx.cn": "https://quay.io",
+  "gcr.qfcx.cn": "https://gcr.io",
+  "k8s-gcr.qfcx.cn": "https://k8s.gcr.io",
+  "k8s.qfcx.cn": "https://registry.k8s.io",
+  "ghcr.qfcx.cn": "https://ghcr.io",
+  "cloudsmith.qfcx.cn": "https://docker.cloudsmith.io",
+  "ecr.qfcx.cn": "https://public.ecr.aws",
 
   // staging
-  "docker-staging.libcuda.so": dockerHub,
+  "docker-staging.qfcx.cn": dockerHub,
 };
 
 function routeByHosts(host) {
